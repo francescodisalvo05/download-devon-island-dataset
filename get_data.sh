eb7752da68881a8ca782f112601b59032f755e2b
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-10 10:21:45 +0100

    Download train data

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# there is a total of 22 scenes
+
+# set the number of training examples
+# to do : make it parametric
+
+mkdir train/
+cd train
+for i in {1..2}
+do
+    if [$i -lt 10]
+    then 
+        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
+    else
+        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
+    fi
+done
+
+
+
 
 
 
