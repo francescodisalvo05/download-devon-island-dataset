5f3c20ec9cd42fa115bf2d7d495c83e643c7fe81
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-10 10:39:02 +0100

    Add validation loop

--- get_data.sh
+++ get_data.sh
@@ -2,6 +2,10 @@
 
 # there is a total of 22 scenes
 
+#############
+# TRAINING
+#############
+
 # set the number of training examples
 n_train=11
 
@@ -17,6 +21,28 @@ for ((i=0; i<n_train; i++)); do
         curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
     fi
 done
+
+#############
+# VALIDATION
+#############
+
+# set the number of validation examples
+n_val=2
+
+cd ..
+mkdir train/
+cd train
+
+for ((j=$i; j<n_val+; i++)); do
+
+    if [ $j -lt 10 ]
+    then 
+        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
+    else
+        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
+    fi
+done
+
 
 
 
