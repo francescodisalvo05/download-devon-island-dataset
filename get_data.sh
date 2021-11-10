7b4b2fcb3535d91ca2e94bb35583aa8eeac42a52
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-10 10:59:51 +0100

    Add validation and test set

--- get_data.sh
+++ get_data.sh
@@ -2,9 +2,14 @@
 
 # there is a total of 22 scenes
 
+# to do : parametrize the kind of images that we want
+# -- they can be : 
+
 #############
 # TRAINING
 #############
+
+echo "-- Downloading train images..."
 
 # set the number of training examples
 n_train=11
@@ -16,9 +21,11 @@ for ((i=0; i<n_train; i++)); do
 
     if [ $i -lt 10 ]
     then 
-        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
+        echo 'scene_0'$i'.zip'
+        #curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
     else
-        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
+        echo 'scene_'$i'.zip'
+        #curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
     fi
 done
 
@@ -26,24 +33,52 @@ done
 # VALIDATION
 #############
 
+echo "-- Downloading validation scenes..."
+
 # set the number of validation examples
 n_val=2
 
 cd ..
-mkdir train/
-cd train
+mkdir val/
+cd val
 
-for ((j=$i; j<n_val+; i++)); do
+for ((j=$i; j<n_train+n_val; j++)); do
 
     if [ $j -lt 10 ]
     then 
-        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
+        echo 'scene_0'$j'.zip'
+        curl -o './val/scene_0'$j'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$j'.zip'
     else
-        curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
+        echo 'scene_'$j'.zip'
+        curl -o './val/scene_0'$j'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$j'.zip'
     fi
 done
 
 
+#############
+# TESTING
+#############
+
+echo "-- Downloading test scenes..."
+
+# set the number of testing examples
+n_test=2
+
+cd ..
+mkdir test/
+cd test
+
+for ((k=$j; k<n_train+n_val+n_test; k++)); do
+
+    if [ $k -lt 10 ]
+    then 
+        echo 'scene_0'$k'.zip'
+        curl -o './train/scene_0'$k'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$k'.zip'
+    else
+        echo 'scene_'$k'.zip'
+        curl -o './train/scene_0'$k'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$k'.zip'
+    fi
+done
 
 
 
