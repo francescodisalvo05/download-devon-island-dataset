a0c2ad5c0960182647b0ea26140cbca3797879b2
francescodisalvo05 <francesco.disalvo99@gmail.com>
2021-11-10 10:37:19 +0100

    Parametric for loop

--- get_data.sh
+++ get_data.sh
@@ -3,20 +3,20 @@
 # there is a total of 22 scenes
 
 # set the number of training examples
-# to do : make it parametric
+n_train=11
 
 mkdir train/
 cd train
-for i in {1..2}
-do
-    if [$i -lt 10]
+
+for ((i=0; i<n_train; i++)); do
+
+    if [ $i -lt 10 ]
     then 
         curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s0'$i'.zip'
     else
         curl -o './train/scene_0'$i'.zip' 'ftp://asrl3.utias.utoronto.ca/Devon-Island-Rover-Navigation/rover-traverse/grey-rectified-512x384/grey-rectified-512x384-s'$i'.zip'
     fi
 done
-
 
 
 
